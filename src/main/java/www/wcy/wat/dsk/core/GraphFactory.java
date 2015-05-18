797e117f21828b71c163a9827383e099daaf9fda
KRRySS <jedynakpoczta@gmail.com>
2015-05-18 17:07:29 +0200

    Wstępna struktura dla projektu.

---
+++ src/main/java/www/wcy/wat/dsk/core/GraphFactory.java
@@ -0,0 +1,7 @@
+package www.wcy.wat.dsk.core;
+
+/**
+ * Created by Krzysztof Jedynak on 2015-05-18.
+ */
+public class GraphFactory {
+}
---
+++ src/main/java/www/wcy/wat/dsk/utils/CommonVariables.java
@@ -0,0 +1,7 @@
+package www.wcy.wat.dsk.utils;
+
+/**
+ * Created by Krzysztof Jedynak on 2015-05-18.
+ */
+public class CommonVariables {
+}
