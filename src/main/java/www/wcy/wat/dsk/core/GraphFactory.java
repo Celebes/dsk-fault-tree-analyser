1b43eb93ab85011466dcecdeece9b2f1210acf57
KRRySS <jedynakpoczta@gmail.com>
2015-05-18 20:55:39 +0200

    Dodanie biblioteki jgraphX oraz uporządkowanie zależności.

--- src/main/java/www/wcy/wat/dsk/core/GraphFactory.java
+++ src/main/java/www/wcy/wat/dsk/core/GraphFactory.java
@@ -1,7 +1,20 @@
 package www.wcy.wat.dsk.core;
 
+import org.jgrapht.Graph;
+import org.jgrapht.VertexFactory;
+import org.jgrapht.generate.GraphGenerator;
+
+import javax.swing.*;
+import java.util.Map;
+
 /**
  * Created by Krzysztof Jedynak on 2015-05-18.
  */
-public class GraphFactory {
+
+public class GraphFactory extends JApplet implements GraphGenerator{
+
+    @Override
+    public void generateGraph(Graph target, VertexFactory vertexFactory, Map resultMap) {
+
+    }
 }
